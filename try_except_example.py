35b2f19c75f6963b51a65d97e10a7c72dc3a3be9
fauk74 <65495422+fauk74@users.noreply.github.com>
2022-02-11 10:51:56 +0100

    Create try_except_example.py

---
+++ try_except_example.py
@@ -0,0 +1,16 @@
+
+try:
+    file= open("a_file.txt")
+    a_dict={"key":"value"}
+    print(a_dict["key"])
+except FileNotFoundError:
+    file=open("a_file.txt","w")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exists")
+else:
+    content=file.read()
+    print(content)
+
+finally:
+    file.close()
+    print("File was closed.")
